4f8f79d91ab9b68ce20d51059cf6bd761d7896ee
David Antunes <803679+dvdantunes@users.noreply.github.com>
2018-11-05 01:48:12 -0300

    [setup] App generation from cookiecutter-flask-minimal template

---
+++ kanikumo_engine/kanikumo_engine/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('kanikumo_engine.default_settings')
+app.config.from_envvar('KANIKUMO_ENGINE_SETTINGS')
+
+if not app.debug:
+    import logging
+    from logging.handlers import TimedRotatingFileHandler
+    # https://docs.python.org/3.6/library/logging.handlers.html#timedrotatingfilehandler
+    file_handler = TimedRotatingFileHandler(os.path.join(app.config['LOG_DIR'], 'kanikumo_engine.log'), 'midnight')
+    file_handler.setLevel(logging.WARNING)
+    file_handler.setFormatter(logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
+    app.logger.addHandler(file_handler)
+
+import kanikumo_engine.views
