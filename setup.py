9e2404b02657722a7b01243258bc079fdfe41639
David Antunes <dvdantunes@gmail.com>
2018-11-05 03:10:48 -0300

    [setup] Added travis configuration for CI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kanikumo_engine',
-    version='1.0',
+    version='0.1',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
