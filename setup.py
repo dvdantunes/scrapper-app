a4dbbc503bba4f42e54907dcb0be5c171e68bb0b
David Antunes <803679+dvdantunes@users.noreply.github.com>
2018-11-06 05:19:00 -0300

    [setup] Adding app dependencies + Improvements on setup and build processes

--- setup.py
+++ setup.py
@@ -1,13 +1,64 @@
+import os
 from setuptools import setup, find_packages
 
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+
+description = 'A simple web scrapper'
+
+long_description = ''
+with open(os.path.join(CURRENT_PATH, 'README.md'), 'r') as f:
+    long_description = f.read()
+
+
+requirements = [
+        'Flask==1.0.2',
+        'Flask-SQLAlchemy==2.3.1',
+        'Flask-Migrate==2.3.0',
+        'psycopg2==2.6.1',
+        'service_identity==17.0.0',
+        'pyasn1==0.4.4',
+        'scrapy==1.5',
+        'gunicorn==19.9.0',
+        'Sphinx==1.8.1',
+        'sphinx-rtd-theme==0.4.2',
+        'pytest-flask==0.14.0',
+        'coverage==4.5.1',
+    ]
+
+extras_require = {
+    'docs': [
+        'Sphinx==1.8.1',
+        'sphinx-rtd-theme==0.4.2'
+    ],
+    'tests': [
+        'pytest-flask==0.14.0',
+        'coverage==4.5.1',
+    ]
+}
+
+
 setup(
-    name='kanikumo_engine',
-    version='0.2',
-    long_description=__doc__,
-    packages=find_packages(),
+    name='kanikumo-engine',
+    version='0.3',
+
+    author='David Antunes',
+    author_email='dvdantunes@gmail.com',
+
+    url='https://github.com/dvdantunes/scrapper-app',
+    project_urls={
+        "Source": "https://github.com/dvdantunes/scrapper-app",
+        "Tracker": "https://github.com/dvdantunes/scrapper-app/issues",
+    },
+    description=description,
+    long_description=long_description,
+
+    packages=find_packages(exclude=['docs', 'tests']),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'flask',
-    ],
+
+    install_requires=requirements,
+    extras_require=extras_require,
 )
