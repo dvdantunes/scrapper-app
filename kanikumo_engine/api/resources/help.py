c43ba3b4bdd0539bf30de1b6a46a3e1aa2b68abb
David Antunes <dvdantunes@gmail.com>
2018-11-08 15:11:26 -0300

    [code] Adding fliget generated kanikumo text to help endpoint

--- kanikumo_engine/api/resources/help.py
+++ kanikumo_engine/api/resources/help.py
@@ -17,7 +17,13 @@ class Help(Resource):
             None
         """
 
-        msg = 'Kon\'nichiwa! This is Kanikumo Engine scrapper api help. \n\n'\
+        msg = '  _  __           _ _                            _____             _             \n'\
+            ' | |/ /__ _ _ __ (_) | ___   _ _ __ ___   ___   | ____|_ __   __ _(_)_ __   ___ \n'\
+            ' | \' // _` | \'_ \\| | |/ / | | | \'_ ` _ \\ / _ \\  |  _| | \'_ \\ / _` | | \'_ \\ / _ \\ \n'\
+            ' | . \\ (_| | | | | |   <| |_| | | | | | | (_) | | |___| | | | (_| | | | | |  __/ \n'\
+            ' |_|\\_\\__,_|_| |_|_|_|\\_\\\\__,_|_| |_| |_|\\___/  |_____|_| |_|\\__, |_|_| |_|\\___| \n'\
+            '                                                             |___/  \n'\
+            'Kon\'nichiwa! This is Kanikumo Engine scrapper api help. \n\n'\
             'Available end-points: \n'\
             '  /help                             Shows this help \n'\
             '  /mercadopublico/big-purchases     Get last big purchases from mercadopublico.cl \n'
