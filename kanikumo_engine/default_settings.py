1cc75a44beefcc78d6bfac1418f6f7674821b13f
David Antunes <dvdantunes@gmail.com>
2018-11-08 05:35:35 -0300

    [docs] Improvements on settings docs

--- kanikumo_engine/default_settings.py
+++ kanikumo_engine/default_settings.py
@@ -1,2 +1,10 @@
-DEBUG = False  # make sure DEBUG is off unless enabled explicitly otherwise
-LOG_DIR = '.'  # create log files in current working directory
+# App default settings
+
+# Secret key for security reasons
+SECRET_KEY = 'UAujh/GuR3##85nFH~eF:S~:DE`%]>'
+
+# make sure DEBUG is off unless enabled explicitly otherwise
+DEBUG = False
+
+# create log files in current working directory
+LOG_DIR = '.'
