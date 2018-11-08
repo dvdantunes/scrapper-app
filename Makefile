5fb5a558c777fe976bd5dd0e2409ef087336b76e
David Antunes <803679+dvdantunes@users.noreply.github.com>
2018-11-08 02:32:19 -0300

    [config] Adding local support for gunicorn + Initialization improvements
    - Improvements on makefile initialization
    - Added gunicorn support, setup and logging

--- Makefile
+++ Makefile
@@ -22,9 +22,18 @@ install: venv
 	venv/bin/python setup.py develop
 
 
-# Run the app
+# Run the app on development env
+run-dev: install
+	WEB_SERVER=flask-wsgi FLASK_APP=kanikumo_engine KANIKUMO_ENGINE_SETTINGS=../settings.cfg venv/bin/flask run
+
+
+# Run the app on production env
 run: install
-	FLASK_APP=kanikumo_engine KANIKUMO_ENGINE_SETTINGS=../settings.cfg venv/bin/flask run
+	WEB_SERVER=gunicorn FLASK_APP=kanikumo_engine KANIKUMO_ENGINE_SETTINGS=../settings.cfg venv/bin/gunicorn kanikumo_engine:app -p /tmp/kanikumo_engine.pid -b 127.0.0.1:5000 --access-logfile logs/access.log --log-file logs/error.log -D
+
+# Stop the app
+stop:
+	kill `cat /tmp/kanikumo_engine.pid`
 
 
 # Execute test suite
--- kanikumo_engine/__init__.py
+++ kanikumo_engine/__init__.py
@@ -1,17 +1,28 @@
 import os
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('kanikumo_engine.default_settings')
 app.config.from_envvar('KANIKUMO_ENGINE_SETTINGS')
 
-if not app.debug:
-    import logging
+
+# Logging errors
+if os.environ.get('WEB_SERVER') == 'gunicorn':
+
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+else:
+    # Default rotating logs
     from logging.handlers import TimedRotatingFileHandler
+
     # https://docs.python.org/3.6/library/logging.handlers.html#timedrotatingfilehandler
     file_handler = TimedRotatingFileHandler(os.path.join(app.config['LOG_DIR'], 'kanikumo_engine.log'), 'midnight')
     file_handler.setLevel(logging.WARNING)
     file_handler.setFormatter(logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
     app.logger.addHandler(file_handler)
+
 
 import kanikumo_engine.views
