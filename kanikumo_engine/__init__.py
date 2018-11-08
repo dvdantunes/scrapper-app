165dac02502d77133b79fa4ee22182708f651361
David Antunes <dvdantunes@gmail.com>
2018-11-08 05:35:06 -0300

    [config] Adding Sentry error logging

--- kanikumo_engine/__init__.py
+++ kanikumo_engine/__init__.py
@@ -1,7 +1,18 @@
-import os
-import logging
+import os, logging
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 from flask import Flask
 
+
+# Sentry logging integration
+sentry_sdk.init(
+    dsn="https://d2da0368423246d7a8185b0c57fae3d8@sentry.io/1316056",
+    integrations=[FlaskIntegration()]
+)
+
+
+
+# Create app and set configuration
 app = Flask(__name__)
 app.config.from_object('kanikumo_engine.default_settings')
 app.config.from_envvar('KANIKUMO_ENGINE_SETTINGS')
@@ -9,6 +20,7 @@ app.config.from_envvar('KANIKUMO_ENGINE_SETTINGS')
 
 # Logging errors
 if os.environ.get('WEB_SERVER') == 'gunicorn':
+    # gunicorn logging
 
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
@@ -23,6 +35,8 @@ else:
     file_handler.setLevel(logging.WARNING)
     file_handler.setFormatter(logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
     app.logger.addHandler(file_handler)
+
+
 
 
 import kanikumo_engine.views
