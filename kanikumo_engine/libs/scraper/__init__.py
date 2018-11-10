3d08a65f3f403a38bb32850872e21d29fb5b37c8
David Antunes <803679+dvdantunes@users.noreply.github.com>
2018-11-10 08:18:12 -0300

    [code] First version of lua script to get mercadopubclico.cl data

--- kanikumo_engine/libs/scraper/__init__.py
+++ kanikumo_engine/libs/scraper/__init__.py
@@ -1,10 +1,10 @@
 import os, logging, json
-from scrapy import spiderloader
+from scrapy import spiderloader, signals
 from scrapy.crawler import CrawlerProcess, CrawlerRunner
+from scrapy.xlib.pydispatch import dispatcher
 from scrapy.settings import Settings
 from scrapy.utils.log import configure_logging
 from twisted.internet import reactor
-
 
 
 
@@ -55,16 +55,66 @@ def scraper_crawl_job(spider_name):
 
     # Configure a crawler process for the spider and runs it
     try:
-        process = CrawlerProcess(crawl_settings)
-        process.crawl(spider_class)
-        process.start()
+
+        # Trying to stop reactor
+        # @see https://stackoverflow.com/questions/23775915/how-to-know-reactor-is-running-or-not-in-python
+        # @see https://doc.scrapy.org/en/latest/topics/practices.html#run-scrapy-from-a-script
+        # def reactor_stop():
+        #     if reactor.running:
+        #         reactor.stop()
+        #         logging.info('[scrapy] Reactor stopped')
+
+        # dispatcher.connect(reactor_stop, signal=signals.spider_closed)
+
+        # process = CrawlerProcess(crawl_settings)
+        # process.crawl(spider_class)
+
+        # logging.info('[scrapy] Running reactor...')
+        # process.start()
+
 
         # configure_logging({'LOG_FORMAT': '[%(levelname)s]: %(message)s'})
         # runner = CrawlerRunner(crawl_settings)
 
         # deferred = runner.crawl(spider_class)
-        # deferred.addBoth(lambda _: reactor.stop())
-        # reactor.run()
+        # deferred.addBoth(lambda _: reactor_stop)
+        # if reactor.callWhenRunning(lambda: None) is not None:
+        #     logging.info('[scrapy] Running reactor...')
+        #     reactor.run()
+        # else:
+        #     logging.info('[scrapy] Reactor already running ...')
+
+        import requests
+
+        headers={
+            'content-type' : 'application/json'
+        }
+        r = requests.post('http://localhost:8050/execute',
+                    json = {
+                        "timeout":90,
+                        "response_body":False,
+                        "images":1,
+                        "resource_timeout":60,
+                        "save_args":[
+
+                        ],
+                        "wait":60,
+                        "load_args":{
+
+                        },
+                        "viewport":"1024x768",
+                        "lua_source":"function main(splash)\r\n  local url = splash.args.url\r\n  assert(splash:go(url))\r\n  assert(splash:wait(1))\r\n\r\n  -- go back 1 month in time and wait a little (1 second)\r\n  assert(splash:runjs(\"document.querySelector('#heaFecha label[for=chkFecha]').click();\"))\r\n  assert(splash:wait(0.2))\r\n\r\n  assert(splash:runjs(\"document.querySelector('#txtFecha1').value = '10-10-2018';\"))\r\n  assert(splash:runjs(\"document.querySelector('#txtFecha2').value = '10-11-2018';\"))\r\n  assert(splash:runjs(\"document.querySelector('#btnBusqueda').click();\"))\r\n  assert(splash:wait(1))\r\n\r\n\r\n  local pages = {}\r\n  pages['1'] = {html = splash:html(), png = splash:png()}\r\n\r\n  assert(splash:runjs(\"document.querySelector('#PaginadorBusqueda__TblPages td:nth-child(3) div').click();\"))\r\n  assert(splash:wait(1))\r\n\r\n  pages['2'] = {html = splash:html(), png = splash:png()}\r\n\r\n  -- return result as a JSON object\r\n  return pages\r\nend",
+                        "png":1,
+                        "har":1,
+                        "html5_media":False,
+                        "html":1,
+                        "url":"http://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=9",
+                        "http_method":"GET",
+                        "render_all":False
+                    },)
+                    # headers=headers)
+
+        return r.json()
 
     except Exception as e:
         logging.exception('Error while executing crawling process')
