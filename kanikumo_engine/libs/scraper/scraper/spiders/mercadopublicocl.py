2e2c1ccf7067ad9c4659d43063013e1cbc6d91f4
David Antunes <dvdantunes@gmail.com>
2018-11-10 16:48:06 -0300

    [backup] Saving original crawler approach, replaced due to a scrapy-splash bug

--- kanikumo_engine/libs/scraper/scraper/spiders/mercadopublicocl.py
+++ kanikumo_engine/libs/scraper/scraper/spiders/mercadopublicocl.py
@@ -1,4 +1,4 @@
-import scrapy
+import os, scrapy
 from scrapy_splash import SplashRequest, SplashFormRequest
 
 
@@ -29,10 +29,80 @@ class MercadoPublicoClBigPurchasesSpider(scrapy.Spider):
             [type] -- [description]
         """
 
-        for url in self.start_urls:
-            yield SplashRequest(url=url,
-                callback=self.parse,
-                endpoint='render.html')
+        script = """
+        function main(splash)
+          local url = splash.args.url
+          assert(splash:go(url))
+          assert(splash:wait(1))
+
+          -- set dates and submit form
+          assert(splash:runjs("document.querySelector('#heaFecha label[for=chkFecha]').click();"))
+          assert(splash:wait(0.2))
+
+          assert(splash:runjs("document.querySelector('#txtFecha1').value = '%s';"))
+          assert(splash:runjs("document.querySelector('#txtFecha2').value = '%s';"))
+          assert(splash:runjs("document.querySelector('#btnBusqueda').click();"))
+          assert(splash:wait(3))
+
+          -- get pages content
+          local pages = {}
+          pages['page1'] = {html = splash:html()}
+
+          -- check paginator #2 and get page 2
+          assert(splash:runjs("document.querySelector('#PaginadorBusqueda__TblPages td:nth-child(3) div').click();"))
+          assert(splash:wait(3))
+          pages['page2'] = {html = splash:html()}
+
+          return pages
+        end
+        """ % ('10-10-2018', '10-11-2018')
+
+
+        # yield SplashRequest('http://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=9',
+        #     self.parse,
+        #     args={
+        #         # time to wait for updates after page is loaded
+        #         # 'wait' : 50,
+
+        #         # 'timeout' : 60,
+        #         'lua_source': script
+        #     },
+        #     cache_args=['lua_source'],
+        #     endpoint='execute')
+
+
+        yield scrapy.Request('http://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=9',
+            self.parse,
+            meta={
+                'splash': {
+                    "args":{
+                        # time to wait for updates after page is loaded
+                        "timeout":90,
+                        "response_body":False,
+                        "images":1,
+                        "resource_timeout":0,
+                        "save_args":[
+
+                        ],
+                        "wait":30,
+                        "load_args":{
+
+                        },
+                        "viewport":"1024x768",
+                        'lua_source': script,
+                        "png":1,
+                        "har":1,
+                        "html5_media":False,
+                        "html":1,
+                        "url":"http://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=9",
+                        "http_method":"GET",
+                        "render_all":False
+                    },
+                    'cache_args':['lua_source'],
+                    'endpoint':'execute'
+                },
+            },
+        )
 
 
 
@@ -45,7 +115,13 @@ class MercadoPublicoClBigPurchasesSpider(scrapy.Spider):
             response {[type]} -- [description]
         """
 
+        # current_path = os.path.abspath(os.path.dirname(__file__))
+        # output_file = os.path.join(current_path, 'output2.json')
+
+        # with open(output_file, "w+") as text_file:
+        #     text_file.write(response.body_as_unicode())
+        #     pass
+
         yield {
-            'title' : response.css('title::text').extract_first(),
-            'label' : response.css('#heaFecha label[for="chkFecha"]::text').extract_first(),
+            'data' : response.data
         }
